a4b508e828d083cbdfd44e37a124ea7eb4a9fa70
MostafaEldalil <mostafa.eldalil2@gmail.com>
2021-03-20 15:45:28 +0200

    All lecture Features

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $allPosts = [
+            ['id' => 1, 'title' => 'laravel', 'posted_by' => 'Mostafa', 'created_at' => '2021-03-20'],
+            ['id' => 2, 'title' => 'PHP', 'posted_by' => 'Mahmoud', 'created_at' => '2021-03-21'],
+            ['id' => 3, 'title' => 'JS', 'posted_by' => 'Ali', 'created_at' => '2021-03-22'],
+        ];
+        return view('posts.index', [
+            'posts' => $allPosts
+        ]);
+    }
+
+    public function show($postId)
+    {
+        $post = ['id' => 1, 'title' => 'laravel', 'description' => 'laravel is awsome framework', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-20'];
+        return view('posts.show', [
+            'post' => $post,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        return redirect()->route('posts.index');
+    }
+}
